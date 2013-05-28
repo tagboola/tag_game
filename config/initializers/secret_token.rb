e4c8aa4b29ddafcd4abb390842b17911269c4318
Tunde Agboola <tun.agboola@gmail.com>
2013-05-28 07:40:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TagGame::Application.config.secret_token = '16a2dc23e81fe0e6df82dae511d9810567fe19383ce9f9924b34ac33352edcaed8c1962810efc333764b8c48eac5f9ed73a38b058730445117eecef6dc4f5f70'
