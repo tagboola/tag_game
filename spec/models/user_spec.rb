e041d5c43ac2c934334f7ecddeb3e375481c2a2d
Tunde Agboola <tun.agboola@gmail.com>
2013-05-28 10:10:47 -0400

    More user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,14 @@ describe User do
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
   it {should respond_to(:remember_me)}
+  it {should respond_to(:reset_password_token)}
+  it {should respond_to(:reset_password_sent_at)}
+  it {should respond_to(:remember_created_at)}
+  it {should respond_to(:sign_in_count)}
+  it {should respond_to(:current_sign_in_at)}
+  it {should respond_to(:last_sign_in_at)}
+  it {should respond_to(:current_sign_in_ip)}
+  it {should respond_to(:last_sign_in_ip)}
   
   it {should be_valid}
   
@@ -24,5 +32,29 @@ describe User do
     before {@user.password = ""}
     it {should_not be_valid}
   end
+  
+  describe "when sign in count does not default to 0" do
+    its(:sign_in_count) {should == 0}
+  end
+  
+  describe "when email is not unique" do
+    before do
+      dup_user = @user.dup
+      dup_user.email = @user.email.upcase
+      dup_user.save
+    end
+    it {should_not be_valid}
+  end
+  
+  describe "when reset password token is not unique" do
+    before do
+      @user.reset_password_token = "test"
+      dup_user = @user.dup
+      dup_user.save
+    end
+    it {should_not be_valid}
+  end
+   
+  
 
 end
